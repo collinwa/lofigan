ce81a785d04f2e456015f1ef347c2b499f8311b1
collinwa <clw2180@columbia.edu>
2020-08-12 01:14:03 -0400

    Somewhat finished script for train_keras. Need to stack input tensors before training

--- train.py
+++ train.py
@@ -1,48 +1,70 @@
+import random
 import tensorflow as tf
 import scipy.sparse as sparse
-import random
-import os
 from tensorflow.keras.optimizers import Adam
+from tensorflow.keras.activations import softmax
+from parameters import *
 
 def random_cut(audio, duration):
-    return random.randint(0, audio.shape[0] - duration)
+        return random.randint(0, audio.shape[0] - duration)
+
+def get_max_t(audio, duration):
+    return audio.shape[0] - (audio.shape[0] % duration) - 1
 
 if __name__ == '__main__':
-    duration = 16384
-    epochs = 50
-    lr=1e-3
-    batch_size=1
     loss = tf.nn.softmax_cross_entropy_with_logits
 
     training_set = sorted(os.listdir(base_dir))[0:140]
     validation_set = sorted(os.listdir(base_dir))[140:]
+
     optimizer = Adam(learning_rate=lr)
-    model = WaveNet(batch=batch_size, duration=duration)
-    end_cut_size = model.final_dim
+    model = WaveNet(batch=batch_size, duration=duration, max_dilation=max_dilation)
     cur_losses = []
+    total_counter=0
+    time_steps = 1
+
     with tf.device("/GPU:0"):
-      for epoch in range(epochs):
-          for i, f in enumerate(training_set):
-              print('Loading %s in TimeStep %i' % (f, i))
-              audio = sparse.load_npz(os.path.join(base_dir, f))
-              print('loaded')
+        for epoch in range(epochs):
+            for i, f in enumerate(training_set):
+                print('Loading %s in TimeStep %i' % (f, i))
 
-              start = random_cut(audio, duration)
-              audio = audio[start:start+duration,:].toarray()
-              audio = audio[None,:,:]
+                # load the whole sparse audio matrix
+                audio = sparse.load_npz(os.path.join(base_dir, f))
 
-              audio = tf.convert_to_tensor(audio)
-              
-              with tf.GradientTape() as tape:
-                  logits = model(audio)
-                  cur_loss = loss(audio[:,-end_cut_size:,:], logits)
+                # maximum timestep that we can predict with
+                max_t = get_max_t(audio, duration)
 
-              grads = tape.gradient(cur_loss, model.trainable_weights)
-              optimizer.apply_gradients(zip(grads, model.trainable_weights))
+                # get the previous timestep to predict the next
+                # prev_audio = tf.convert_to_tensor(audio[0:duration,:].toarray())[None,:,:]
+                jump_size=random.randint(10, 22050)
+                this_its = 0
+                for j in range(0, (max_t-1), jump_size):
+                    # align the original y
+                    prev_audio = audio[j:j+duration,:]
+                    prev_audio = tf.convert_to_tensor(prev_audio.toarray())[None,:,:]
 
-              cur_loss = tf.stop_gradient(cur_loss)
-              cur_losses.append(tf.math.reduce_mean(cur_loss))
+                    # shift the step size to be aligned + 100 timesteps
+                    next_y = audio[j+pow(2, max_dilation)+time_steps:j+pow(2, max_dilation)+pred_size+time_steps,:]
+                    next_y = tf.convert_to_tensor(next_y.toarray())[None,:,:]
 
-              print('Epoch {} Timestep {} Current Loss {}'.format(epoch, i, tf.math.reduce_mean(cur_loss)))
+                    # run backwards
+                    with tf.GradientTape() as tape:
+                            logits = model(prev_audio)
+                            cur_loss = loss(next_y, logits)
 
-          model.save(os.path.join(save_dir, 'model_epoch_%i' % epoch), save_format='tf')
+                    # compute gradients and backprop
+                    grads = tape.gradient(cur_loss, model.trainable_weights)
+                    optimizer.apply_gradients(zip(grads, model.trainable_weights))
+
+                    # track the mean loss 
+                    cur_loss = tf.stop_gradient(cur_loss)
+                    cur_losses.append(tf.math.reduce_mean(cur_loss))
+                    total_counter += 1
+                    this_its += 1
+                    if total_counter % 100 == 0:
+                        print('Epoch {} Timestep {} Current Loss {}'.format(epoch, total_counter, tf.math.reduce_mean(cur_loss)))
+
+                model.save(os.path.join(save_dir, 'wavenet_tmpsave_withjumps'), save_format='tf')
+
+        model.save(os.path.join(save_dir, 'model_epoch_%i' % epoch), save_format='tf')
+
---
+++ train_keras.py
@@ -0,0 +1,45 @@
+import random
+import os
+import scipy.sparse as sparse
+import tensorflow as tf 
+from tensorflow.keras.optimizers import Adam
+from WaveNetKerasFunctional import create_model
+
+random.seed(1337)
+
+if __name__ == '__main__':
+    # define hyperparameters
+    seq_len = 132300  # 3 second long input
+    in_ch = 256  # 256 input channels
+    causal_ch = 512  # first causal conv will produce 512 channels
+    stack_ch = 128  # each causal stack will produce 128
+    scaled_ch = 128
+    filter_dim = 2  # filter dimension
+    max_dilation = 10  # maximum dilation
+    n_stacks = 4  # number of causal conv stacks
+    time_shift = 1  # timesteps to shift 
+
+    # generate all file list and randomly permute
+    data_dir = "./sparse_matrices"
+    all_files = os.listdir(data_dir)
+    all_files = [os.path.join(data_dir, x) for x in all_files]
+    random.shuffle(all_files)
+    
+    # training / test split
+    val_pct = 0.1
+    train_pct = 1. - val_pct     
+    train_qty = int(len(os.listdir(data_dir)) * train_pct)
+    training_set = all_files[:train_qty]
+    validation_set = all_files[train_qty:]
+    
+    # create the model
+    model, pred_size = create_model(seq_len=seq_len, 
+            in_ch=in_ch,
+            causal_ch=causal_ch,
+            stack_ch=stack_ch,
+            scaled_ch=scaled_ch,
+            filter_dim=filter_dim,
+            max_dilation=max_dilation,
+            n_stacks=n_stacks)
+    print(pred_size)
+
